10e073e9a96e9a3e78c585e25c09245072f3fcf7
artem <zalivonka02@gmail.com>
2022-11-09 21:12:58 +0300

    first commit

---
+++ chanel/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for chanel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.sessions import SessionMiddlewareStack
+from django.core.asgi import get_asgi_application
+import chat1.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chanel.settings')
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        SessionMiddlewareStack(URLRouter(chat1.routing.websocket_urlpatterns)))
+})
